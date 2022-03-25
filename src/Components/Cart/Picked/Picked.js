c8b3fc711b2c92cc76677b585dcc7f39ac687cea
RKZeebon <rkzeebon@gmail.com>
2022-03-26 01:24:37 +0600

    choose button edited

--- src/Components/Cart/Picked/Picked.js
+++ src/Components/Cart/Picked/Picked.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './Picked.css'
 
 const Picked = (props) => {
-    const { name } = props.item;
+    const { name, img } = props.item;
     return (
+
         <div>
-            <h2>Randomly Picked:</h2>
-            <h3>{name}</h3>
+            <h3>Randomly Picked:</h3>
+            <div className='picked-item'>
+                <img src={img} alt="" />
+                <h4>{name}</h4>
+            </div>
         </div>
     );
 };
